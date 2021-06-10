762ff669e9c86892db1e5ad47d1271f4298073c6
JAGAGANESH <67314150+JAGAGANESH@users.noreply.github.com>
2021-06-10 11:04:31 +0530

    Create dam_random_values.js

---
+++ TCE_IT_FOURT SEMESTER_PM/Water Quality Monitoring/dam_random_values.js
@@ -0,0 +1,19 @@
+function Change10Seconds() {
+    setInterval(RandomValues_ChangeValues(), 10000);
+}
+
+function RandomValues_ChangeValues() {
+    var old_dam_ph = dam_data[i].Ph;
+    var old_dam_level = dam_data[i].Level; 
+    var old_dam_temperature = dam_data[i].Temperature;
+    var old_dam_flow = dam_data[i].Flow;
+    var new_dam_ph = 100;
+    var new_dam_level = 100;
+    var new_dam_temperature = 100;
+    var new_dam_flow = 100;
+    old_dam_ph = new_dam_ph;
+    old_dam_level = new_dam_level;
+    old_dam_temperature = new_dam_temperature;
+    old_dam_flow = new_dam_flow;
+    setInterval(RandomValues_ChangeValues, 10000);
+}
