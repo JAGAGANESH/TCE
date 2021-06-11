8bb7882c7c1c50e507ddea1a64a6c537da8992c7
JAGAGANESH <67314150+JAGAGANESH@users.noreply.github.com>
2021-06-11 07:54:11 +0530

    Update dam_change_values.js

--- TCE_IT_FOURT SEMESTER_PM/Water Quality Monitoring/dam_change_values.js
+++ TCE_IT_FOURT SEMESTER_PM/Water Quality Monitoring/dam_change_values.js
@@ -1,6 +1,10 @@
 function RepeatChangeFunction() {    
     ChangeValues_SendToDatabase();
-    setInterval(RepeatChangeFunction, 10000);
+    setInterval(RepeatChangeFunction, 15000);
+}
+function RepeatChangeAllFunction() {    
+    ChangeAllValues_SendToDatabase();
+    setInterval(RepeatChangeAllFunction, 30000);
 }
 
 function ChangeValues_SendToDatabase() {
@@ -17,6 +21,21 @@ function ChangeValues_SendToDatabase() {
     });
 }
 
+function ChangeAllValues_SendToDatabase() {
+    for(i=0;i<=dam_data.length;i++) {
+        var dam_name = dam_data[i].Name;
+        var new_dam_ph = RandomValue_Ph();
+        var new_dam_level = RandomValue_Level();
+        var new_dam_temperature = RandomValue_Temperature();
+        var new_dam_flow = RandomValue_Flow()
+        firebase.database().ref("Fourth Semester/Project Management/Water Quality Management/TN DAMS/"+dam_name).set({
+            Ph: new_dam_ph,
+            Level: new_dam_level,
+            Temperature: new_dam_temperature,
+            Flow: new_dam_flow,
+        });
+    }
+}
 function RandomValue_Ph() {
     var rv_ph = Math.floor(Math.random() * 1000) + 500;
     return rv_ph;
