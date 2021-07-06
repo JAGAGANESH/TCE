97ffbc67ab9ebb110d4bde75b97492b9002a77d2
JAGAGANESH <67314150+JAGAGANESH@users.noreply.github.com>
2021-07-06 10:12:55 +0530

    Delete TCE_IT_FOURT SEMESTER_PM/TollGate Management System directory

--- TCE_IT_FOURT SEMESTER_PM/TollGate Management System/ConnectToDatabase.php
+++
@@ -1,9 +0,0 @@
-<?php
-$connection = oci_connect('system','Jaga12345','localhost/DuJa');
-If (!$connection) {
-    echo "oops, Database not connected to server!";
-}
-else {
-}
-oci_close($connection);
-?>
