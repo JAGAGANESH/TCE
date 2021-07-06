180a0023bd7752f68955294bc1d339ece0f10771
JAGAGANESH <67314150+JAGAGANESH@users.noreply.github.com>
2021-07-06 17:12:28 +0530

    Create Home.php

---
+++ TCE_IT_FOURTH SEMESTER_DBMSL/TollGate Management System/Home.php
@@ -0,0 +1,19 @@
+<?php
+include("ConnectToDatabase.php");
+?>
+<!DOCTYPE HTML>
+<html>
+<title>Tollgate Management System</title>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href="index.css">
+<script src="index.js"></script>
+</head>
+<body>
+<title1>
+<t1><b>TOLL TAX MANAGEMENT SYSTEM</b></t1></br>
+<t2 style="color:white;font-size:30px;">Department of Transport Service</t2>
+</title1>
+
+</body>
+</html>
