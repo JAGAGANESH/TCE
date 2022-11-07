093e0290e520a12b31cf52f24bed3c14c37fe33b
JAGAGANESH <67314150+JAGAGANESH@users.noreply.github.com>
2022-11-07 08:44:52 +0530

    Delete TCE/LMCS directory

--- TCE/LMCS/JavaScript/index.js
+++
@@ -1,5 +0,0 @@
-function Rating() {
-    document.getElementById("star1").classList.add("checked");
-    document.getElementById("star2").classList.add("checked");
-    document.getElementById("star3").classList.add("checked");
-}
--- TCE/LMCS/README.md
+++
@@ -1,4 +0,0 @@
-# Labour management in construction Site
-
-## Prototype
-https://jagaganesh.github.io/TCE/TCE/LMCS/Login.html
